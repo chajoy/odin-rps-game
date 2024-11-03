71d92d59cbcd379b395c7870d6e192c02b1b0269
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-03 20:11:47 +0000

    Add function to play a round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0,
+    computerScore = 0;
+
 const responses = [`rock`, `paper`, `scissors`];
 
 //Returns a random string from the 'responses' array
@@ -19,3 +22,33 @@ const getPlayerChoice = function ()
     return input;
 }
 
+const playRound = function (playerChoice, computerChoice)
+{
+    let result;
+
+    if(playerChoice === computerChoice)
+    {
+        result = `draw`;
+    }else if(playerChoice === `paper` && computerChoice === `scissors` ||
+            playerChoice === `rock` && computerChoice === `paper` ||
+            playerChoice === `scissors` && computerChoice === `rock`)
+    {
+        result = `lose`;
+    }else if(playerChoice === `paper` && computerChoice === `rock` ||
+            playerChoice === `rock` && computerChoice === `scissors` ||
+            playerChoice === `scissors` && computerChoice === `paper`)
+    {
+        result = `win`;
+    }else
+    {
+        console.error(`Error: could not complete round`);
+    }
+
+    return `You ${result}`;
+}
+
+//Temporary play function to call playRound with getPlayerChoice and getComputerChoice
+const play = function ()
+{
+    return playRound(getPlayerChoice(), getComputerChoice());
+}
