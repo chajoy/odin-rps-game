ad9d03645e215c24e05fe8205f10a1db25be8e15
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-03 19:57:57 +0000

    Add function for receiving player choice

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const getComputerChoice = function ()
 {
     return responses[Math.floor(Math.random() * 3)];
 }
+
+//Returns player choice. Asks for input until it's either `rock`,`paper` or `scissors`
+const getPlayerChoice = function () 
+{
+    let input;
+
+    do{
+        input = prompt(`Enter Choice:`);
+        input = input.toLowerCase();
+    }while(!responses.includes(input));
+
+    return input;
+}
+
