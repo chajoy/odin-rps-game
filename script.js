4fee00b39b9e6e0d9995eead7d7fdbdc46a978bd
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2024-11-03 19:49:58 +0000

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,7 @@
+const responses = [`rock`, `paper`, `scissors`];
+
+//Returns a random string from the 'responses' array
+const getComputerChoice = function ()
+{
+    return responses[Math.floor(Math.random() * 3)];
+}
